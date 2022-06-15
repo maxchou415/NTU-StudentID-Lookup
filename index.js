bdcac1d39fef02e73bbf50eb04977214b88fee37
Max Chou <yihsunmaxchou@icloud.com>
2022-06-16 01:35:06 +0800

    Used RegEx to check format

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const departmentList = require('./data/departments.json')
 
 module.exports = ({ studentId = '' }) => {
-  const studentIdArray = [...studentId]
-  if (studentIdArray.length !== 9) {
+  if (!/^[a-zA-Z]{1}[0-9]{2}[a-zA-Z0-9]{6}$/.test(studentId)) {
     throw new Error('INVALID_FORMAT')
   }
+  const studentIdArray = [...studentId]
 
   let departmentCode = `${studentIdArray[3]}${studentIdArray[4]}${studentIdArray[5]}0`
   departmentCode = departmentCode.toUpperCase()
